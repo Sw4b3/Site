23c4e1036237902cc891147a303e5feaef65c438
AndrewSchwabe <swabe@live.co.za>
2018-03-11 13:46:00 +0200

    Update
    -Improved All pages

--- Ecommerce website/public_html/Script.js
+++ Ecommerce website/public_html/Script.js
@@ -3,18 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
-window.onscroll = function () {
-    myFunction();
+
+window.onload = function () {
+    showSlides(1);
 };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
